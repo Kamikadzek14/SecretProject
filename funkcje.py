ca3dd2da2c4801d3275672946621c1fc71e394a9
miloszgost <miloszgost@wp.pl>
2021-11-30 21:52:35 +0100

    Test

---
+++ funkcje.py
@@ -0,0 +1,16 @@
+# energia kinetyczna, potencjalna i zasada zachowania energii
+
+
+def kinetic_energy(mass, velocity):
+    return mass*velocity**2/2
+
+
+G = 6.674*10**(-11)
+
+
+def potential_energy(mass1, mass2, distance):
+    return G*mass1*mass2/distance
+
+
+def
+
