b58fb00802fe5b85b3f1f7a5736fc746effb8ee2
Kamikadzek14 <jakub.rozyc@gmail.com>
2021-11-30 22:32:55 +0100

    test

--- funkcje2.py
+++ funkcje2.py
@@ -6,3 +6,17 @@ def momentum(mass, velocity):
 
 def density(mass, volume):
     return (mass/volume)
+
+# TO JEST ŹLE :)
+
+def momentum_rule(mass1, mass2, velocity1, velocity2):
+
+    if #object_1rect.colliderect(object2rect):
+        if abs(object2rect.top - object_1rect.bottom) < collision_tollerance:
+                velocity1[1] = -velocity1[1]
+        if abs(object2rect.bottom - object_1rect.top) < collision_tollerance:
+                velocity1[1] = -velocity1[1]
+        if abs(object2rect.right - object_1rect.left) < collision_tollerance:
+                velocity2[0] = -velocity2[0]
+        if abs(object2rect.left - object_1rect.right) < collision_tollerance:
+                velocity2[0] = -velocity2[0]
--- global variables.py
+++ global variables.py
@@ -10,5 +10,12 @@ density = 0
 distance = 0
 kinetic_energy = 0
 potential_energy = 0
+radius = 0
+circumference = 0
+collision_tollerance = 5
+kinetic_1 = 0
+kinetic_2 = 0
+potential_1 = 0
+potential_2 = 0
 
 G = 6.674*10**(-11)
