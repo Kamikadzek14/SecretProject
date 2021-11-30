5330dc37c205315c6d1b10604420aa1b6a89bb7d
miloszgost <miloszgost@wp.pl>
2021-11-30 22:04:13 +0100

    Test

--- funkcje.py
+++ funkcje.py
@@ -2,15 +2,14 @@
 
 
 def kinetic_energy(mass, velocity):
-    return mass*velocity**2/2
+    return (mass*velocity**2)/2
 
 
 G = 6.674*10**(-11)
 
 
 def potential_energy(mass1, mass2, distance):
-    return G*mass1*mass2/distance
+    return (G*mass1*mass2)/distance
 
 
 def
-
