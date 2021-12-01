d5a07e8c189958abd3025137e3eeb03dd5589e48
Kamikadzek14 <jakub.rozyc@gmail.com>
2021-12-01 14:42:26 +0100

    test

--- output_bouncing_balls.py
+++ output_bouncing_balls.py
@@ -13,9 +13,9 @@ print("Secret_Project_X")
 
 pygame.init()
 
-size = width, height = 1400, 700
-speed = [2, 3]
-speed2 = [1,1]
+size = width, height = 1400, 200
+speed = [10, 0]
+speed2 = [-10,0]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -24,7 +24,10 @@ screen = pygame.display.set_mode(size)
 ball1 = pygame.image.load(os.path.join('images', 'circle.png'))
 ball2 = pygame.image.load(os.path.join('images', 'circle.png'))
 ball1rect = ball1.get_rect()
+ball1rect.center = 32,100
 ball2rect = ball2.get_rect()
+ball2rect.center = 1300, 100
+# w//2, h//2
 
 clock = pygame.time.Clock()
 
@@ -47,32 +50,32 @@ while 1:
         speed2[1] = -speed2[1]
     clock.tick(60)
 # Collision with themselves
-#     collision_tollerance = 30
-#     if ball1rect.colliderect(ball2rect):
-#         if abs(ball2rect.top - ball1rect.bottom) < collision_tollerance:
-#             speed[1] = -speed[1]
-#         if abs(ball2rect.bottom - ball1rect.top) < collision_tollerance:
-#             speed[1] = -speed[1]
-#         if abs(ball2rect.right - ball1rect.left) < collision_tollerance:
-#             speed[0] = -speed[0]
-#         if abs(ball2rect.left - ball1rect.right) < collision_tollerance:
-#             speed[0] = -speed[0]
+    collision_tollerance = 20
+    if ball1rect.colliderect(ball2rect):
+        if abs(ball2rect.top - ball1rect.bottom) < collision_tollerance:
+            speed[1] = -speed[1]
+        if abs(ball2rect.bottom - ball1rect.top) < collision_tollerance:
+            speed[1] = -speed[1]
+        if abs(ball2rect.right - ball1rect.left) < collision_tollerance:
+            speed[0] = -speed[0]-1
+        if abs(ball2rect.left - ball1rect.right) < collision_tollerance:
+            speed[0] = -speed[0]-1
 
-    # if ball2rect.colliderect(ball1rect):
-    #     if abs(ball1rect.top - ball2rect.bottom) < collision_tollerance:
-    #         speed2[1] = -speed2[1]
-    #     if abs(ball1rect.bottom - ball2rect.top) < collision_tollerance:
-    #         speed2[1] = -speed2[1]
-    #     if abs(ball1rect.right - ball2rect.left) < collision_tollerance:
-    #         speed2[0] = -speed2[0]
-    #     if abs(ball1rect.left - ball2rect.right) < collision_tollerance:
-    #         speed2[0] = -speed2[0]
+    if ball2rect.colliderect(ball1rect):
+        if abs(ball1rect.top - ball2rect.bottom) < collision_tollerance:
+            speed2[1] = -speed2[1]
+        if abs(ball1rect.bottom - ball2rect.top) < collision_tollerance:
+            speed2[1] = -speed2[1]
+        if abs(ball1rect.right - ball2rect.left) < collision_tollerance:
+            speed2[0] = -speed2[0]-1
+        if abs(ball1rect.left - ball2rect.right) < collision_tollerance:
+            speed2[0] = -speed2[0]-1
 
 
 
     screen.fill(black)
     screen.blit(ball1, ball1rect)
-    screen.blit(ball2, ball2rect)
+    screen.blit(ball2, ball2rect,)
     #screen.blit(ball2, ball2rect , to_pygame((450,100)))
 
     pygame.display.flip()
