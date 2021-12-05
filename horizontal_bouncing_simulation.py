0bed6d8575bd1576c7903c0a5080f85bedb6cc72
Kamikadzek14 <jakub.rozyc@gmail.com>
2021-12-05 21:52:39 +0100

    test

--- horizontal_bouncing_simulation.py
+++ horizontal_bouncing_simulation.py
@@ -1,23 +1,24 @@
 import sys
 import os
-import numpy
+import numpy as np
 import matplotlib
 import pygame
 
 pygame.init()
 
 size = width, height = 1400, 200
-speed = [10, 0]
-map(float, speed)
-speed = [6, 0]
-speed2 = [-5,0]
+
+speed = [2, 0.0]
+#list(np.float_(speed))   - will not work because the move function does not support float  :(
+speed2 = [-1,0]
+
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
 
-BigBall = pygame.image.load(os.path.join('images', 'circle.png'))
-SmallBall = pygame.image.load(os.path.join('images', 'circle2.png'))
+BigBall = pygame.image.load(os.path.join('images', 'BigBall.png'))
+SmallBall = pygame.image.load(os.path.join('images', 'SmallBall.png'))
 BigBallrect = BigBall.get_rect()
 BigBallrect.center = 32,100
 SmallBallrect = SmallBall.get_rect()
@@ -25,14 +26,16 @@ SmallBallrect.center = 1300, 100
 
 
 # Parameters of balls
-massBigBall =input("Insert mass of BigBall:")
-massSmallBall =input("Insert mass of SmallBall:")
-while massBigBall < massSmallBall:
-    print("massBigBall can not be smaller than massSmallBall\nTry Again")
-    massBigBall = input("Insert mass of BigBall:")
-    massSmallBall = input("Insert mass of SmallBall:")
-    if massBigBall > massSmallBall:
-        break
+# massBigBall =input("Insert mass of BigBall:")
+# massSmallBall =input("Insert mass of SmallBall:")
+#
+#
+# while massBigBall < massSmallBall:
+#     print("massBigBall can not be smaller than massSmallBall\nTry Again")
+#     massBigBall = input("Insert mass of BigBall:")
+#     massSmallBall = input("Insert mass of SmallBall:")
+#     if massBigBall > massSmallBall:
+#         break
 
 
 
@@ -42,8 +45,13 @@ clock = pygame.time.Clock()
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+    clock.tick(60)
 
-# Collision with walls
+
+
+
+
+    # Collision with walls
     BigBallrect = BigBallrect.move(speed)
     if BigBallrect.left < 0 or BigBallrect.right > width:
         speed[0] = -speed[0]
@@ -52,7 +60,24 @@ while 1:
     if SmallBallrect.left < 0 or SmallBallrect.right > width:
         speed2[0] = -speed2[0]
 
-# Collision with themselves
+    # Collision with themselves
 
-collision_tollerance = 40
+    collision_tollerance = 30
 
+
+
+
+
+
+
+
+
+
+
+
+
+    screen.fill(black)
+    screen.blit(BigBall, BigBallrect)
+    screen.blit(SmallBall, SmallBallrect,)
+
+    pygame.display.flip()
